7e4cb616d91d93eb9f1cce4d8ca41e177fd3e1cc
fyhao <fyhao1@gmail.com>
2020-07-31 14:04:16 +0800

    Revert "test with fail test"
    This reverts commit 22dd421fed1783852299de46698ae9da1c3dc980.

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class LibraryTest {
 	
     @Test public void testSomeLibraryMethod() {
-        assertTrue("someLibraryMethod should return 'true'", false);
+        assertTrue("someLibraryMethod should return 'true'", true);
     }
     
 }
