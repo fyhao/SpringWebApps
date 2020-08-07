19196eebc715d0ef0063742d8e2ef6d72cd41538
fyhao <fyhao1@gmail.com>
2020-08-07 17:23:29 +0000

    add springboottest

--- src/test/java/TestingWebApplicationTests.java
+++ src/test/java/TestingWebApplicationTests.java
@@ -1,8 +1,5 @@
 
-
-import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -13,26 +10,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.fyhao.springwebapps.*;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = SpringWebMain.class)
 public class TestingWebApplicationTests {
 
 	@LocalServerPort
 	private int port;
-	
-	@Autowired
-	private TestController controller;
-	
-	@Test
-	public void contextLoads() {
-		assertThat(controller).isNotNull();
-	}
 
 	@Autowired
-	private TestRestTemplate restTemplate;
-
+    private TestRestTemplate restTemplate;
+    
+    @Autowired
+    TestController test;
+    @Test
+    public void contextLoads() {
+        
+    }
 	@Test
 	public void greetingShouldReturnDefaultMessage() throws Exception {
-		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/test",
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/test/",
 				String.class)).contains("Hello, World");
 	}
 }
