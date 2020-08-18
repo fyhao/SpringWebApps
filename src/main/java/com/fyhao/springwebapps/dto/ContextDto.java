b4dc78db05ddd86fe4c746e140f7478194525c24
fyhao <fyhao1@gmail.com>
2020-08-19 00:49:16 +0800

    Manually merge ui branch server side change to master

---
+++ src/main/java/com/fyhao/springwebapps/dto/ContextDto.java
@@ -0,0 +1,22 @@
+package com.fyhao.springwebapps.dto;
+
+import java.io.Serializable;
+
+public class ContextDto implements Serializable {
+
+	String key;
+	String value;
+	public String getKey() {
+		return key;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}
